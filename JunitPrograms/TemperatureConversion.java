4aada1fbf15b6b21c7d85c191c0d18af45f35c3b
ksoundarya8 <ksoundarya4@gmail.com>
2019-12-12 10:54:58 +0530

    first commit

--- JunitPrograms/TemperatureConversion.java
+++ JunitPrograms/TemperatureConversion.java
@@ -1,5 +1,18 @@
 package com.bridgelabs.JunitPrograms;
 
-public class TemperatureConversion {
+import com.bridgelabs.util.utilclass;
 
+public class TemperatureConversion {
+	
+
+	public static void main(String[] args)
+	{
+		System.out.println ("Enter temperature to be converted");
+		int temp = utilclass.InputInteger();
+		
+		System.out.println("Enter 1 to convert Celsius to Fahrenheit /n Enter 2 to convert Fahrenheit to Celsius /n");
+		int ch = utilclass.InputInteger();
+		
+		utilclass.TempConversion(temp, ch);
+	}
 }
