021bb80f50bf7d6a1c8a91c5f518b4c2d8ee92db
ksoundarya8 <ksoundarya4@gmail.com>
2019-12-14 17:20:45 +0530

    14-12-2019 evening

---
+++ AlgorithmPrograms/Anagram.java
@@ -0,0 +1,41 @@
+package com.bridgelabs.AlgorithmPrograms;
+
+import java.util.Arrays;
+
+import com.bridgelabs.util.utilclass;
+
+public class Anagram {
+	
+	public static boolean isAnagram(String s1, String s2)
+	{
+		char[] ch1 = s1.toCharArray();
+		char[] ch2 = s2.toCharArray();
+		if(s1.length()!=s2.length())
+			return false;
+		else
+		{
+			Arrays.sort(ch1);
+			
+			Arrays.parallelSort(ch2);
+			
+			for(int i = 0 ;i<s1.length(); i++)
+			{
+				if(ch1[i] != ch2[i])
+					return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("Enter  two string to check for anagram");
+		String s1 = utilclass.InputString();
+		String s2 = utilclass.InputString();
+		
+		if(isAnagram(s1,s2))
+			System.out.println("Strings are anagram");
+		else
+			System.out.println("String are not anagram");
+	}
+}
